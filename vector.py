d1d565a1daebd14c69426e5f256cc259eabfd101
Naman Kumar <namankumar1170@gmail.com>
2026-05-29 20:02:00 +0530

    commit

--- vector.py
+++ vector.py
@@ -4,26 +4,30 @@ from langchain_core.documents import Document
 import os
 import pandas as pd
 
-df=pd.read_csv("medicine_dataset.csv")
+df = pd.read_csv("medicine_dataset.csv")
 print(df.columns)
 
-db_location="./chrome_langchain_db"
-embedding=OllamaEmbeddings(model="nomic-embed-text")
-add_documents=not os.path.exists(db_location)
+db_location = "./chrome_langchain_db"
+embedding = OllamaEmbeddings(model="nomic-embed-text")
+add_documents = not os.path.exists(db_location)
 
 if add_documents:
-    documents=[]
-    ids=[] 
+    documents = []
+    ids = [] 
     for i, row in df.iterrows():
-        document=Document(
+        document = Document(
             page_content=str(row["id"]),
-            metadata={"name": str(row["name"]), "substitute0": str(row["substitute0"]), "substitute1": str(row["substitute1"]), "substitute2": str(row["substitute2"])}
+            metadata={
+                "name": str(row["name"]),
+                "substitute0": str(row["substitute0"]),
+                "substitute1": str(row["substitute1"]),
+                "substitute2": str(row["substitute2"]),
+            }
         )
         documents.append(document)
-        ids.append(str(i))  
-        documents.append(document) 
+        ids.append(str(i))
 
-vector_store=Chroma(
+vector_store = Chroma(
     collection_name="medicine_collection",
     embedding_function=embedding,
     persist_directory=db_location
@@ -31,9 +35,8 @@ vector_store=Chroma(
 if add_documents:
     vector_store.add_documents(documents, ids=ids)
 
-#connection between llm and vector store
+# connection between llm and vector store
 
-retriever=vector_store.as_retriever(
+retriever = vector_store.as_retriever(
     search_kwargs={"k": 5}
-    )                                       
-                                 
+    )
